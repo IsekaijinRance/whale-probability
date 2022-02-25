21b62a3aed1b7ca923fc427c99293946b93ddf91
Isekaijin Rance <isekaijin.tech@gmail.com>
2022-02-25 07:49:24 -0500

    Initial commit. Created main program file.

---
+++ twinwhale.py
@@ -0,0 +1,20 @@
+def main():
+    number_of_iterations = 1000
+
+    ### Print the problem statement
+    print(
+    '''
+    Suppose you have two whales
+    You know that at least one of them is a male whales
+
+    What is the probability that both whales are male?
+    '''
+    )
+
+    
+
+
+if __name__ == "__main__":
+    print(f"Running as twinwhale.py as: {__name__}")
+
+    main()
