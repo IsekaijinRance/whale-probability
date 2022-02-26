565807c84e4fcbcbdb29bf440c50b27544004cbb
Isekaijin Rance <isekaijin.tech@gmail.com>
2022-02-25 23:26:14 -0500

    Added formated text for the result.

--- twinwhale.py
+++ twinwhale.py
@@ -16,17 +16,18 @@ def calculateWhaleCombinations(number_of_times, mode = 0):
     order_roll = mode
     
     for i in range(number_of_times):
-        if mode is 0:
+        if mode == 0:
             order_roll = (random.randint(1,100)% 2) + 1
 
-        if order_roll is 1:
+        if order_roll == 1:
             results["male-" + rollWhale()] += 1
 
-        if order_roll is 2:
+        if order_roll == 2:
             results[rollWhale() + "-male"] += 1
 
     return results
-    ##! END OF calculateWhaleCombinations(number_of_times, mode = 0):
+
+##! END OF calculateWhaleCombinations(number_of_times, mode = 0):
 
 def main():
     number_of_iterations = 1000
@@ -41,9 +42,18 @@ def main():
     '''
     )
 
-    print(calculateWhaleCombinations(number_of_iterations, 0))
+    ### Activating single threaded mode.
+    print(f"Using a single thread to run a simulation to calculate {number_of_iterations} pairs of whales.")    
+    result = calculateWhaleCombinations(number_of_iterations, 0)
+
+    ###Finished computing so print the result of the data.
+    print(f'Finished computing {number_of_iterations} pair of whales and these are the results:')
+    for p in result:
+        print(f"{p} pair: \t {result[p]} \t ({round(result[p]/number_of_iterations*100, 2)}%)")
+    print('')
+
+##! END OF def main():
 
 if __name__ == "__main__":
-    print(f"Running as twinwhale.py as: {__name__}")
-
     main()
+    
